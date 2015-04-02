5eee6611ffa8e6c0c0c09a9f947e12c366f7af8c
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-04-02 15:54:02 +0200

    added grouping functionality

--- smdc_perftests/performance_tests/analyze.py
+++ smdc_perftests/performance_tests/analyze.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 import smdc_perftests.performance_tests.test_cases as test_cases
 
 
-def analyze_files(results_files, name_fm=None):
+def analyze_files(results_files, name_fm=None, grouping_f=None):
     """
     Takes a list of results file names and makes a few plots
     contrasting the measured times
@@ -39,17 +39,27 @@ def analyze_files(results_files, name_fm=None):
         if set a function that gets the name of the results and
         returns a more meaningful name. This is useful if the names of
         the results are very long or verbose.
+    grouping_f: function ,optional
+        can be used to assign groups according to the name of the
+        results. Gets the name and returns a string.
     """
     if name_fm is None:
         name_fm = lambda x: x
+    if grouping_f is None:
+        grouping_f = lambda x: 'means'
 
-    d = {'means': []}
+    d = {}
     names = []
 
     for fname in results_files:
         res = test_cases.TestResults(fname)
-        names.append(name_fm(res.name))
-        d['means'].append(res.mean)
+        name = name_fm(res.name)
+        if name not in names:
+            names.append(name)
+        group = grouping_f(res.name)
+        if group not in d:
+            d[group] = []
+        d[group].append(res.mean)
 
     df = pd.DataFrame(d, index=names)
     ax = df.plot(kind='bar')
@@ -62,11 +72,18 @@ def esa_cci_name_formatter(n):
     chunking = parts[2]
     return chunking.split(',')[0]
 
+
+def esa_cci_grouping(n):
+    parts = n.split('_')
+    rtype = parts[3]
+    return '-'.join(rtype.split('-')[-2:])
+
 if __name__ == '__main__':
     import glob
     import os
 
     path = os.path.join(
         "/media", "sf_D", "SMDC", "performance_tests", "CCI_testdata", "results")
-    fs = glob.glob(os.path.join(path, "*daily-img.nc"))
-    analyze_files(fs, name_fm=esa_cci_name_formatter)
+    fs = glob.glob(os.path.join(path, "*.nc"))
+    analyze_files(fs, name_fm=esa_cci_name_formatter,
+                  grouping_f=esa_cci_grouping)
