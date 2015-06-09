741ad2d9852e1e2f32ed1d5391c82dffb766adfe
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-06-09 14:58:15 +0200

    update mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ from sphinx import apidoc
 
 import mock
 
-MOCK_MODULES = ['numpy', 'matplotlib', 'pygeogrids', 'pandas', 'scipy', 'netCDF4']
+MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'pygeogrids.grids', 'pandas', 'scipy.stats', 'netCDF4']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
